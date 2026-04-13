5d5328d97a3f2ae42098959701be9846dd7cce17
Josue <AO-Alumno@duoc.acad>
2026-04-13 12:33:43 -0400

    feat/main: activiti three questions

--- app.py
+++ app.py
@@ -8,4 +8,39 @@
 #Solicitar el ingreso de 3 notas por pantalla, luego calcular el promedio de las 3 notas (cada nota tiene la misma ponderación), finalmente indicar con una salida de pantalla “Aprobado” en el caso de que el promedio sea igual o mayor a 4.0.
 
 
+# 1
+
+edad = int(input("ingresa tu edad:\n"))
+
+if edad >= 18:
+    print("eres mayor de edad")
+else:
+    print("eres menor de edad")
+
+usuario = input("inrgese su usuario:\n")
+contraseña = input("ingrese su contraseña:\n")
+
+if usuario == "pedro" and contraseña == "1234":
+    print("Bienvenido Pedro")
+elif usuario == "angel" and contraseña == "a4s1":
+    print("bienvenido angel")
+else:
+    print("usuario o contraseña incorrectos")
+
+nota1 = float(input("ingrese su primera nota:\n"))
+nota2 = float(input("ingrese su segunda nota:\n"))
+nota3 = float(input("ingrese su tercera nota:\n"))
+
+promedio = (nota1 + nota2 + nota3) / 3
+print(f"tu promedio es: {promedio}")
+
+if promedio > 4.0:
+    print("aprobado")
+else:
+    print("reprobado")
+
+
+
+
+
 
